59d929d19afb8cafcc58526eac0a3e0e8dda0f2e
Tony Bowden <tony@tmtm.com>
2021-08-29 13:05:04 +0300

    add scraper for official site

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -7,17 +7,17 @@ require 'pry'
 class MemberList
   class Member
     def name
-      noko.css('.name').text.tidy
+      noko.css('a').children.map(&:text).join(' ').tidy
     end
 
     def position
-      noko.css('.position').text.tidy
+      noko.css('p').text.tidy
     end
   end
 
   class Members
     def member_container
-      noko.css('.member')
+      noko.css('#min-vc-text')
     end
   end
 end
