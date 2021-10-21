9c5e807252c0d95bdbd19ee6b1fc94b807b0634a
Tony Bowden <tony@tmtm.com>
2021-10-21 13:02:23 +0300

    Don't report on the PM not being in the Ministers list

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,5 +3,11 @@
 
 require 'every_politician_scraper/comparison'
 
+# Not listed in the list of ministers, but part of the cabinet
+SKIP = [
+  ['---', 'Nikol Pashinyan', 'Prime Minister of Armenia']
+].freeze
+
 diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+                                         .reject { |row| SKIP.include? row }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
