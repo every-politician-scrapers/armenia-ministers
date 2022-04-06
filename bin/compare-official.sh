bc9f0323a0fb15e00c3e5879b857db241ef12111
Tony Bowden <tony@tmtm.com>
2022-04-06 18:59:44 +0300

    less verbose comparisons

--- bin/compare-official.sh
+++ bin/compare-official.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bundle exec ruby comparisons/official/scraper.rb | ifne tee comparisons/official/scraped.csv
-bundle exec ruby comparisons/official/wikidata.rb meta.json | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid | ifne tee comparisons/official/wikidata.csv
+bundle exec ruby comparisons/official/scraper.rb > comparisons/official/scraped.csv
+bundle exec ruby comparisons/official/wikidata.rb meta.json | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid > comparisons/official/wikidata.csv
 bundle exec ruby comparisons/official/diff.rb | tee comparisons/official/diff.csv
