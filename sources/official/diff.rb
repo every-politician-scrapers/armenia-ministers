1e27982d2241e92df84a8b9578e61010913fec09
Tony Bowden <tony@tmtm.com>
2022-06-17 13:09:23 +0300

    update to DecoratedComparison

--- sources/official/diff.rb
+++ sources/official/diff.rb
@@ -8,6 +8,6 @@ SKIP = [
   ['---', 'Nikol Pashinyan', 'Prime Minister of Armenia']
 ].freeze
 
-diff = EveryPoliticianScraper::NulllessComparison.new('wikidata.csv', 'scraped.csv').diff
+diff = EveryPoliticianScraper::DecoratedComparison.new('wikidata.csv', 'scraped.csv').diff
                                          .reject { |row| SKIP.include? row }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
